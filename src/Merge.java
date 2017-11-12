d7417622699fbee4181f88702f6e717e5d6863a0
richousrick <rikkeypaal@gmail.com>
2017-11-12 06:07:28 +0000

    Bulk Update
    Merge:
    new options added
    prints current progess
    generates MergeThreads for each file
    
    MergeThread:
    changed to runnable
    now parses, and generates list of methods for each class in the file it handles

--- src/Merge.java
+++ src/Merge.java
@@ -1,9 +1,13 @@
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.antlr.v4.parse.ANTLRParser.throwsSpec_return;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
@@ -13,8 +17,10 @@ import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
-import parse.DummyPlugin;
+import merge.MergeThread;
 import parse.PluginInterface;
+import parse.parser.java.JavaPlugin;
+import ref.Helper;
 
 
 /**
@@ -27,7 +33,10 @@ public class Merge {
 	String language = "";
 	boolean recursive = false;
 	int maxThreads = 1;
-	PluginInterface plugin = new DummyPlugin();
+	PluginInterface plugin = new JavaPlugin();
+	private int minMatch;
+	private double minPer;
+	private double minPerDelta;
 	
 	/**
 	 * Initializes the Merge class
@@ -37,14 +46,30 @@ public class Merge {
 		parseOptions(args);
 		// get list of files
 		ArrayList<File> files = getFiles(new File(path));
+		maxThreads = Math.min(maxThreads, files.size());
 		// dish out to threads
-		System.out.println(Arrays.toString(files.toArray(new File[files.size()])));
 		
 		ExecutorService executor = Executors.newFixedThreadPool(maxThreads);
+		
+		if(Helper.verbose)
+				System.out.println("Reading files and distributing threads");
+		for(File f: files){
+			Helper.printToSTD("Generating thread for "+f.getName(), "Main");
+			MergeThread t = new MergeThread(f, plugin.generateInstance(), minMatch, minPer, minPerDelta);
+			executor.execute(t);
+		}
+		Helper.printToSTD("Thread Assigned, Waiting for completion", "Main");
+		executor.shutdown();
 		
 	}
 	
 	private void parseOptions(String[] args){
+		
+		minMatch = 1;
+		minPer = 0.2;
+		minPerDelta = 0;
+
+		
 		Options options = new Options();
 		
 		Option pathOpt = new Option("p", "path", true, "Path to the file or directory the code is in.");
@@ -59,6 +84,10 @@ public class Merge {
 		
 		options.addOption("r", "recursive", false, "Locate files in specified path recursevly.");
 		options.addOption("t", "max-threads", true, "Maximum number of threads to use.\nThreads used = min(max-threads, num files).\nDefault is 1.");
+		options.addOption("s", "minPer", true, "Minimum percentage of body blocks that must be shared.");
+		options.addOption("d", "minPerDelta", true, "Added to minPer to compare the larger method.\nDefault is 0");
+		options.addOption("m", "minMatch", true, "minimum number of statemetns to consititute a match.\n Defualt is 1");
+		options.addOption("v", "verbose", false, "display logging information");
 		
 		
 		CommandLineParser parser = new DefaultParser();
@@ -77,11 +106,48 @@ public class Merge {
 				recursive = true;
 			}
 			
+			if(cmd.hasOption('v')){
+				Helper.verbose = true;
+			}
+			
 			if(cmd.hasOption('t')){
 				try {
 					maxThreads = Integer.parseInt(cmd.getOptionValue('t'));
 				} catch (NumberFormatException e) {
 					System.out.println("Error: Thread parameter must be a positive integer");
+					showUsageExit(options);
+				}
+			}
+			
+			if(cmd.hasOption('s')){
+				try {
+					minPer = Double.parseDouble(cmd.getOptionValue('s'));
+					if(minPer<=0||minPer>1){
+						throw new NumberFormatException();
+					}
+				} catch (NumberFormatException e) {
+					System.out.println("Error: MinPer parameter must be in range 0>minPer>=1");
+					showUsageExit(options);
+				}
+			}
+			
+			if(cmd.hasOption('d')){
+				try {
+					minPerDelta = Double.parseDouble(cmd.getOptionValue('d'));
+					if(minPerDelta+minPer<=0||minPerDelta+minPer>1){
+						throw new NumberFormatException();
+					}
+				} catch (NumberFormatException e) {
+					System.out.println("Error: MinPerDelta parameter must be in range 0>minPerDelta-minPer>=1");
+					showUsageExit(options);
+				}
+			}
+			
+			if(cmd.hasOption('m')){
+				try {
+					minMatch = Integer.parseInt(cmd.getOptionValue('m'));
+				} catch (NumberFormatException e) {
+					System.out.println("Error: MinMatch parameter must be a positive integer");
 					showUsageExit(options);
 				}
 			}
@@ -103,26 +169,31 @@ public class Merge {
 	
 
 	public static void main(String[] args) {
-		//args = new String[]{"-rl", "Java", "-p", "D:/Work/Coding/Java/CPMerge - Java/src", "-t", "12"};
-		
+		args = new String[]{"-rvl", "Java", "-p", "testcode/", "-t", "12"};
 		Merge m = new Merge(args);
 	}
 	
 	private ArrayList<File> getFiles(File root){
 		ArrayList<File> returnList = new ArrayList<>();
 		if(root.isDirectory()){
+			if(Helper.verbose){
+				System.out.println("Searching for files in \""+root.getAbsolutePath()+"\"");
+			}
 			for(File f: root.listFiles()){
 				if(f.isFile()&&plugin.validfile(f.getName())){
-					System.out.println(f.getName());
+					if(Helper.verbose){
+						System.out.println("found file \"" +f.getName()+"\"");	
+					}
 					returnList.add(f);
 				}else if(recursive){
 					returnList.addAll(getFiles(f));
 				}
 			}
+		}else{
+			returnList.add(root);
 		}
 		return returnList;
 	}
-	
 	
 	
 	/*TODO:
--- src/merge/MergeThread.java
+++ src/merge/MergeThread.java
@@ -14,40 +14,41 @@ import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.ParserRuleContext;
 
+import com.sun.javafx.scene.paint.GradientUtils.Parser;
+import com.sun.prism.impl.VertexBuffer;
+
 import dif.APTEDCostModel;
 import dif.ParserRuleContextNode;
 import distance.APTED;
 import parse.PluginInterface;
 import parse.ResoultionPattern;
+import parse.parser.java.comp.JavaParser.ClassDeclarationContext;
+import parse.parser.java.comp.JavaParser.MethodDeclarationContext;
+import ref.Helper;
 
 /**
  * @author Rikkey Paal
  *
  */
-public class MergeThread extends Thread{
+public class MergeThread implements Runnable{
 	
-	PluginInterface plugin;
+	private PluginInterface plugin;
+	private File f;
+	private String ThreadName;
+	private int minMatch;
+	private double minPer;
+	private double minPerDelta;
 	
 	/**
 	 * 
 	 */
-	public MergeThread(File f, PluginInterface pluginInterface) {
+	public MergeThread(File f, PluginInterface pluginInterface, int minMatch, double minPer, double minPerDelta) {
 		this.plugin = pluginInterface;
-		try {
-			plugin.parse(readFile(f));
-			ArrayList<ParserRuleContext> cst = plugin.getMethods();
-			HashMap<Class<? extends ParserRuleContext>, ResoultionPattern> rules = plugin.getPatterns();
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-			this.interrupt();
-		}
+		this.minMatch = minMatch;
+		this.minPer = minPer;
+		this.minPerDelta = minPerDelta;
+		this.f = f;
 	}
-	
-	private CharStream readFile(File f) throws IOException{
-		return CharStreams.fromPath(f.toPath());
-	}
-
 	
 	private ArrayList<ParserRuleContext[]> getCopies(ArrayList<ParserRuleContext> cst){
 		
@@ -55,11 +56,48 @@ public class MergeThread extends Thread{
 	}
 	
 	private boolean areMatch(ParserRuleContext p1, ParserRuleContext p2){
-		APTED apted = new APTED<APTEDCostModel, ParserRuleContextNode>(new APTEDCostModel());
+		APTED<APTEDCostModel, ParserRuleContext> apted = new APTED<APTEDCostModel, ParserRuleContext>(new APTEDCostModel());
 		apted.computeEditDistance(new ParserRuleContextNode(p1), new ParserRuleContextNode(p2));
-		LinkedList list = apted.computeEditMapping();
+		LinkedList<int[]> list = apted.computeEditMapping();
 		System.out.println(list.toString());
 		
 		return list.size()==0;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public void run() {
+		Helper.printToSTD("Reading file",f.getName());
+		CharStream contents;
+		try {
+			contents = Helper.readFile(f);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+		Helper.printToSTD("Read file, Parsing contents", f.getName());
+		
+		plugin.parse(contents);
+		Helper.printToSTD("File Parsed, Extracting methods", f.getName());
+		ArrayList<ArrayList<ParserRuleContext>> classMethods = plugin.getMethods();
+		
+		if(Helper.test){
+			for(ArrayList<ParserRuleContext> methods:classMethods){
+				String functionName = ((ClassDeclarationContext)methods.get(0).getParent().getParent().getParent().getParent()).IDENTIFIER().getText();
+				String functions = "";
+				for(ParserRuleContext p : methods){
+					functions += ((MethodDeclarationContext)p).IDENTIFIER().toString()+",";
+				}
+				Helper.printToSTD("Found class \""+functionName +"\" containing methods "+functions.substring(0, functions.length()-1));
+			}
+		}
+		
+		Helper.printToSTD("Methods Extracted, Searching for matches", f.getName());
+		//HashMap<Class<? extends ParserRuleContext>, ResoultionPattern> rules = plugin.getPatterns();
+		for(ArrayList<ParserRuleContext> methods: classMethods){
+			
+		}
 	}
 }
