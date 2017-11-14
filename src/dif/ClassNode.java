c906e1e2667c3a45cf84892ab3023847528907f1
richousrick <rikkeypaal@gmail.com>
2017-11-14 04:28:18 +0000

    Started creating visitor to generate AST
    This will generate a AST from the CST generated from ANTLR. The AST is smaller and can be used by APTED to infer distance.

---
+++ src/dif/ClassNode.java
@@ -0,0 +1,125 @@
+package dif;
+
+import java.io.InvalidClassException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Vector;
+
+import org.antlr.v4.runtime.ParserRuleContext;
+
+import com.sun.corba.se.impl.io.TypeMismatchException;
+
+import node.Node;
+
+/**
+ * TODO Annotate class
+ * 
+ * @author 146813
+ */
+public class ClassNode extends Node<ParserRuleContext> {
+
+	private String identifier;
+
+	/**
+	 * Type of data held in node 0:class head 1:method head 2:statement
+	 */
+	private byte type;
+
+	private int size = -1;
+
+	/**
+	 * Initializes the ClassNode class TODO Annotate constructor
+	 * 
+	 * @param nodeData
+	 * @param identifier
+	 * @param type
+	 * 
+	 */
+	public ClassNode(ParserRuleContext nodeData, String identifier, byte type) {
+		super(nodeData);
+		this.identifier = identifier;
+		this.type = type;
+	}
+
+	/**
+	 * Initializes the ClassNode class TODO Annotate constructor
+	 * 
+	 * @param nodeData
+	 * @param identifier
+	 * @param type
+	 * 
+	 */
+	public ClassNode(ParserRuleContext nodeData, String identifier) {
+		super(nodeData);
+		this.identifier = identifier;
+		this.type = (byte) 2;
+	}
+
+	public ArrayList<ClassNode> getChildrenAsCN() throws ClassCastException {
+		ArrayList<ClassNode> nodes = new ArrayList<>();
+		for (Node<ParserRuleContext> child : getChildren()) {
+			nodes.add((ClassNode) child);
+		}
+		return nodes;
+	}
+
+	/**
+	 * @return the identifier
+	 */
+	public String getIdentifier() {
+		return identifier;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public byte getType() {
+		return type;
+	}
+
+	/**
+	 * @param identifier
+	 *            the identifier to set
+	 */
+	public void setIdentifier(String identifier) {
+		this.identifier = identifier;
+	}
+
+	public int recalculateSize() {
+		if (getChildren().size() == 0) {
+			return 1;
+		} else {
+			size = 1;
+			for (ClassNode c : getChildrenAsCN()) {
+				if (c == null)
+					System.out.println("?");
+				size += c.recalculateSize();
+			}
+		}
+		return size;
+	}
+
+	public int getSize() {
+		if (size == -1)
+			return recalculateSize();
+		else
+			return size;
+	}
+
+	public String print(String prefix, boolean isTail) {
+		ArrayList<ClassNode> children = getChildrenAsCN();
+		String str = prefix + (isTail ? "\\-- " : "|-- ") + identifier;
+		for (int i = 0; i < children.size() - 1; i++) {
+			str += "\n" + children.get(i).print(prefix + (isTail ? "    " : "|   "), false);
+		}
+		if (children.size() > 0) {
+			str += "\n" + children.get(children.size() - 1).print(prefix + (isTail ? "    " : "|   "), true);
+		}
+		return str;
+	}
+
+	public String toString() {
+		return print("", true);
+	}
+
+}
---
+++ src/parse/parser/java/comp/JavaParserCustomVisitor.java
@@ -0,0 +1,565 @@
+package parse.parser.java.comp;
+
+import java.util.ArrayList;
+
+import org.antlr.v4.runtime.tree.RuleNode;
+import org.antlr.v4.runtime.tree.TerminalNodeImpl;
+
+import com.sun.org.apache.regexp.internal.recompile;
+
+import dif.ClassNode;
+import parse.parser.java.comp.JavaParser.ArrayInitializerContext;
+import parse.parser.java.comp.JavaParser.BlockContext;
+import parse.parser.java.comp.JavaParser.BlockStatementContext;
+import parse.parser.java.comp.JavaParser.CatchClauseContext;
+import parse.parser.java.comp.JavaParser.ClassBodyContext;
+import parse.parser.java.comp.JavaParser.ClassBodyDeclarationContext;
+import parse.parser.java.comp.JavaParser.ClassDeclarationContext;
+import parse.parser.java.comp.JavaParser.ClassTypeContext;
+import parse.parser.java.comp.JavaParser.CompilationUnitContext;
+import parse.parser.java.comp.JavaParser.ExplicitGenericInvocationContext;
+import parse.parser.java.comp.JavaParser.ExpressionContext;
+import parse.parser.java.comp.JavaParser.ExpressionListContext;
+import parse.parser.java.comp.JavaParser.FloatLiteralContext;
+import parse.parser.java.comp.JavaParser.InnerCreatorContext;
+import parse.parser.java.comp.JavaParser.IntegerLiteralContext;
+import parse.parser.java.comp.JavaParser.LambdaExpressionContext;
+import parse.parser.java.comp.JavaParser.LiteralContext;
+import parse.parser.java.comp.JavaParser.LocalVariableDeclarationContext;
+import parse.parser.java.comp.JavaParser.MemberDeclarationContext;
+import parse.parser.java.comp.JavaParser.MethodDeclarationContext;
+import parse.parser.java.comp.JavaParser.NonWildcardTypeArgumentsContext;
+import parse.parser.java.comp.JavaParser.PrimaryContext;
+import parse.parser.java.comp.JavaParser.PrimitiveTypeContext;
+import parse.parser.java.comp.JavaParser.StatementContext;
+import parse.parser.java.comp.JavaParser.SuperSuffixContext;
+import parse.parser.java.comp.JavaParser.TypeDeclarationContext;
+import parse.parser.java.comp.JavaParser.TypeTypeContext;
+import parse.parser.java.comp.JavaParser.VariableDeclaratorContext;
+import parse.parser.java.comp.JavaParser.VariableInitializerContext;
+
+/**
+ * TODO Annotate class
+ * 
+ * @author 146813
+ */
+public class JavaParserCustomVisitor extends JavaParserBaseVisitor<ClassNode> {
+
+	ArrayList<ClassNode> classes;
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * parse.parser.java.comp.JavaParserBaseVisitor#visitCompilationUnit(parse.
+	 * parser.java.comp.JavaParser.CompilationitContext)
+	 */
+	@Override
+	public ClassNode visitCompilationUnit(CompilationUnitContext ctx) {
+		ClassNode root = new ClassNode(ctx, "root", (byte) 3);
+		classes = new ArrayList<>();
+		for (TypeDeclarationContext type : ctx.typeDeclaration()) {
+			visitTypeDeclaration(type);
+		}
+
+		for (ClassNode c : classes) {
+			root.addChild(c);
+		}
+		return root;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * parse.parser.java.comp.JavaParserBaseVisitor#visitTypeDeclaration(parse.
+	 * parser.java.comp.JavaParser.TypeDeclarationContext)
+	 */
+	@Override
+	public ClassNode visitTypeDeclaration(TypeDeclarationContext ctx) {
+		if (ctx.classDeclaration() != null) {
+			visitClassDeclaration(ctx.classDeclaration());
+		}
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * parse.parser.java.comp.JavaParserBaseVisitor#visitClassDeclaration(parse.
+	 * parser.java.comp.JavaParser.ClassDeclarationContext)
+	 */
+	@Override
+	public ClassNode visitClassDeclaration(ClassDeclarationContext ctx) {
+		ClassNode n = new ClassNode(ctx, ctx.IDENTIFIER().getText(), (byte) 0);
+		for (ClassBodyDeclarationContext cbdx : ctx.classBody().classBodyDeclaration()) {
+			ClassNode dec = visitClassBodyDeclaration(cbdx);
+			if (dec != null) {
+				n.addChild(dec);
+			}
+		}
+		classes.add(n);
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * parse.parser.java.comp.JavaParserBaseVisitor#visitClassBodyDeclaration(
+	 * parse.parser.java.comp.JavaParser.ClassBodyDeclarationContext)
+	 */
+	@Override
+	public ClassNode visitClassBodyDeclaration(ClassBodyDeclarationContext ctx) {
+		// TODO check what i need
+		if (ctx.memberDeclaration() != null) {
+			return visitMemberDeclaration(ctx.memberDeclaration());
+		}
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * parse.parser.java.comp.JavaParserBaseVisitor#visitMemberDeclaration(parse
+	 * .parser.java.comp.JavaParser.MemberDeclarationContext)
+	 */
+	@Override
+	public ClassNode visitMemberDeclaration(MemberDeclarationContext ctx) {
+		if (ctx.methodDeclaration() != null) {
+			return visitMethodDeclaration(ctx.methodDeclaration());
+		} else if (ctx.classDeclaration() != null) {
+			return visitClassDeclaration(ctx.classDeclaration());
+		}
+
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * parse.parser.java.comp.JavaParserBaseVisitor#visitMethodDeclaration(parse
+	 * .parser.java.comp.JavaParser.MethodDeclarationContext)
+	 */
+	@Override
+	public ClassNode visitMethodDeclaration(MethodDeclarationContext ctx) {
+		if (ctx.methodBody().block() != null) {
+			ClassNode methodNode = new ClassNode(ctx, ctx.IDENTIFIER().toString(), (byte) 1);
+			for (ClassNode n : visitBlock(ctx.methodBody().block()).getChildrenAsCN()) {
+				methodNode.addChild(n);
+			}
+			return methodNode;
+		} else {
+			return null;
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * parse.parser.java.comp.JavaParserBaseVisitor#visitBlock(parse.parser.java
+	 * .comp.JavaParser.BlockContext)
+	 */
+	@Override
+	public ClassNode visitBlock(BlockContext ctx) {
+		ClassNode node = new ClassNode(ctx, "block", (byte) 3);
+		for (BlockStatementContext statement : ctx.blockStatement()) {
+			ClassNode statementNode = visitBlockStatement(statement);
+			if (statementNode != null) {
+				node.addChild(statementNode);
+			}
+		}
+		return node;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * parse.parser.java.comp.JavaParserBaseVisitor#visitBlockStatement(parse.
+	 * parser.java.comp.JavaParser.BlockStatementContext)
+	 */
+	@Override
+	public ClassNode visitBlockStatement(BlockStatementContext ctx) {
+		if (ctx.localVariableDeclaration() != null) {
+			return visitLocalVariableDeclaration(ctx.localVariableDeclaration());
+		} else if (ctx.statement() != null) {
+			return visitStatement(ctx.statement());
+		} else {
+			if (ctx.localTypeDeclaration().classDeclaration() != null) {
+				return visitClassDeclaration(ctx.localTypeDeclaration().classDeclaration());
+			} else {
+				return null;
+			}
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * parse.parser.java.comp.JavaParserBaseVisitor#visitStatement(parse.parser.
+	 * java.comp.JavaParser.StatementContext)
+	 */
+	@Override
+	public ClassNode visitStatement(StatementContext ctx) {
+		ClassNode node = new ClassNode(ctx, "Undefined");
+		if (ctx.ASSERT() != null) {
+			node.setIdentifier("Assert");
+			for (ExpressionContext exp : ctx.expression()) {
+				node.addChild(visitExpression(exp));
+			}
+		} else if (ctx.IF() != null) {
+			node.setIdentifier("If");
+			node.addChild(visitParExpression(ctx.parExpression()));
+			for (StatementContext statement : ctx.statement()) {
+				node.addChild(visitStatement(statement));
+			}
+		} else if (ctx.FOR() != null) {
+			node.setIdentifier("For");
+			node.addChild(visitForControl(ctx.forControl()));
+			node.addChild(visitStatement(ctx.statement(0)));
+		} else if (ctx.DO() != null) {
+			node.setIdentifier("Do-While");
+			node.addChild(visitParExpression(ctx.parExpression()));
+			node.addChild(visitStatement(ctx.statement(0)));
+		} else if (ctx.WHILE() != null) {
+			node.setIdentifier("While");
+			node.addChild(visitParExpression(ctx.parExpression()));
+			node.addChild(visitStatement(ctx.statement(0)));
+		} else if (ctx.TRY() != null) {
+			node.setIdentifier("try");
+			if (ctx.resourceSpecification() != null) {
+				node.addChild(visitResourceSpecification(ctx.resourceSpecification()));
+			}
+			node.addChild(visitBlock(ctx.block()));
+			for (CatchClauseContext c : ctx.catchClause()) {
+				node.addChild(visitCatchClause(c));
+			}
+			if (ctx.finallyBlock() != null) {
+				node.addChild(visitFinallyBlock(ctx.finallyBlock()));
+			}
+		} else if (ctx.SWITCH() != null) {
+			node.setIdentifier("Switch");
+			node.addChild(visitParExpression(ctx.parExpression()));
+			// TODO complete
+		} else if (ctx.SYNCHRONIZED() != null) {
+			// TODO complete
+		} else if (ctx.RETURN() != null) {
+			node.setIdentifier("Return");
+			if (ctx.expression(0) != null) {
+				node.addChild(visitExpression(ctx.expression(0)));
+			}
+		} else if (ctx.THROW() != null) {
+			node.setIdentifier("Throw");
+			if (ctx.expression(0) != null) {
+				node.addChild(visitExpression(ctx.expression(0)));
+			}
+		} else if (ctx.statementExpression != null) {
+			return visitExpression(ctx.expression(0));
+		} else {
+			visitChildren(ctx);
+		}
+		// TODO complete
+
+		return node;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see parse.parser.java.comp.JavaParserBaseVisitor#
+	 * visitLocalVariableDeclaration(parse.parser.java.comp.JavaParser.
+	 * LocalVariableDeclarationContext)
+	 */
+	@Override
+	public ClassNode visitLocalVariableDeclaration(LocalVariableDeclarationContext ctx) {
+		ClassNode decNode = new ClassNode(ctx, "declarations");
+		decNode.addChild(visitTypeType(ctx.typeType()));
+		for (VariableDeclaratorContext dec : ctx.variableDeclarators().variableDeclarator()) {
+			decNode.addChild(visitVariableDeclarator(dec));
+		}
+
+		return decNode;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * parse.parser.java.comp.JavaParserBaseVisitor#visitVariableDeclarator(
+	 * parse.parser.java.comp.JavaParser.VariableDeclaratorContext)
+	 */
+	@Override
+	public ClassNode visitVariableDeclarator(VariableDeclaratorContext ctx) {
+		ClassNode node = new ClassNode(ctx, "declaration");
+		node.addChild(new ClassNode(ctx.variableDeclaratorId(), ctx.variableDeclaratorId().getText()));
+		node.addChild(visitVariableInitializer(ctx.variableInitializer()));
+		return node;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * parse.parser.java.comp.JavaParserBaseVisitor#visitVariableInitializer(
+	 * parse.parser.java.comp.JavaParser.VariableInitializerContext)
+	 */
+	@Override
+	public ClassNode visitVariableInitializer(VariableInitializerContext ctx) {
+		if (ctx.arrayInitializer() != null) {
+			return visitArrayInitializer(ctx.arrayInitializer());
+		} else {
+			return visitExpression(ctx.expression());
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * parse.parser.java.comp.JavaParserBaseVisitor#visitArrayInitializer(parse.
+	 * parser.java.comp.JavaParser.ArrayInitializerContext)
+	 */
+	@Override
+	public ClassNode visitArrayInitializer(ArrayInitializerContext ctx) {
+		// TODO Auto-generated method stub
+		return super.visitArrayInitializer(ctx);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * parse.parser.java.comp.JavaParserBaseVisitor#visitExpression(parse.parser
+	 * .java.comp.JavaParser.ExpressionContext)
+	 */
+	@Override
+	public ClassNode visitExpression(ExpressionContext ctx) {
+		// TODO expand implmentation
+		if (ctx.primary() != null) {
+			return visitPrimary(ctx.primary());
+		} else if (ctx.prefix != null) {
+			ClassNode node = new ClassNode(ctx, "pre:" + ctx.prefix.getText());
+			for (ExpressionContext exp : ctx.expression()) {
+				node.addChild(visitExpression(exp));
+			}
+			return node;
+		} else if (ctx.postfix != null) {
+			ClassNode node = new ClassNode(ctx, "post:" + ctx.postfix.getText());
+			for (ExpressionContext exp : ctx.expression()) {
+				node.addChild(visitExpression(exp));
+			}
+			return node;
+		}
+		ClassNode node = new ClassNode(ctx, "tmp");
+		if (ctx.getChild(0) instanceof ExpressionContext) {
+			node.addChild(visitExpression((ExpressionContext) ctx.getChild(0)));
+			if (ctx.getChild(1) instanceof TerminalNodeImpl) {
+				node.setIdentifier(ctx.getChild(1).getText());
+			} else {
+				return super.visitChildren(ctx);
+			}
+
+			if (ctx.getChild(2) instanceof ExpressionContext) {
+				node.addChild(visitExpression((ExpressionContext) ctx.getChild(2)));
+			} else if (ctx.getChild(2) instanceof TypeTypeContext) {
+				node.addChild(visitTypeType((TypeTypeContext) ctx.getChild(2)));
+			} else if (ctx.getChild(2) instanceof ExpressionListContext) {
+				for (ClassNode c : visitExpressionList((ExpressionListContext) ctx.getChild(2)).getChildrenAsCN()) {
+					node.addChild(c);
+				}
+			} else if (ctx.getChild(2) instanceof TerminalNodeImpl) {
+				node.setIdentifier(node.getIdentifier() + " " + ctx.getChild(2).getText());
+			} else if (ctx.getChild(2) instanceof ExplicitGenericInvocationContext) {
+				return super.visitChildren(ctx);
+			} else {
+				return super.visitChildren(ctx);
+			}
+
+			if (ctx.getChild(3) == null) {
+				return node;
+			} else if (ctx.getChild(3) instanceof TerminalNodeImpl) {
+				node.setIdentifier(node.getIdentifier() + " " + ctx.getChild(3).getText());
+			} else if (ctx.getChild(3) instanceof NonWildcardTypeArgumentsContext) {
+				return super.visitChildren(ctx);
+			} else if (ctx.getChild(3) instanceof InnerCreatorContext) {
+				return super.visitChildren(ctx);
+			} else if (ctx.getChild(3) instanceof SuperSuffixContext) {
+				return super.visitChildren(ctx);
+			} else {
+				return super.visitChildren(ctx);
+			}
+
+			if (ctx.getChild(4) == null) {
+				return node;
+			} else if (ctx.getChild(4) instanceof InnerCreatorContext) {
+				return super.visitChildren(ctx);
+			} else if (ctx.getChild(4) instanceof ExpressionContext) {
+				node.addChild(visitExpression((ExpressionContext) ctx.getChild(4)));
+			} else if (ctx.getChild(4) instanceof TerminalNodeImpl) {
+				node.setIdentifier(node.getIdentifier() + " " + ctx.getChild(4).getText());
+			} else {
+				return super.visitChildren(ctx);
+			}
+			return node;
+		} else if (ctx.getChild(0) instanceof TypeTypeContext) {
+
+		} else if (ctx.getChild(0) instanceof TerminalNodeImpl) {
+
+		} else if (ctx.getChild(0) instanceof ClassTypeContext) {
+
+		} else if (ctx.getChild(0) instanceof LambdaExpressionContext) {
+
+		}
+
+		return super.visitChildren(ctx);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * org.antlr.v4.runtime.tree.AbstractParseTreeVisitor#visitChildren(org.
+	 * antlr.v4.runtime.tree.RuleNode)
+	 */
+	@Override
+	public ClassNode visitChildren(RuleNode arg0) {
+		System.err.println("unimplmented stucture " + arg0.getText());
+		return super.visitChildren(arg0);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * parse.parser.java.comp.JavaParserBaseVisitor#visitExpressionList(parse.
+	 * parser.java.comp.JavaParser.ExpressionListContext)
+	 */
+	@Override
+	public ClassNode visitExpressionList(ExpressionListContext ctx) {
+		ClassNode n = new ClassNode(ctx, "ExpressionList");
+		for (ExpressionContext c : ctx.expression()) {
+			n.addChild(visitExpression(c));
+		}
+		return n;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * parse.parser.java.comp.JavaParserBaseVisitor#visitTypeType(parse.parser.
+	 * java.comp.JavaParser.TypeTypeContext)
+	 */
+	@Override
+	public ClassNode visitTypeType(TypeTypeContext ctx) {
+		if (ctx.primitiveType() != null) {
+			return visitPrimitiveType(ctx.primitiveType());
+		} else {
+			return visitClassOrInterfaceType(ctx.classOrInterfaceType());
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * parse.parser.java.comp.JavaParserBaseVisitor#visitPrimary(parse.parser.
+	 * java.comp.JavaParser.PrimaryContext)
+	 */
+	@Override
+	public ClassNode visitPrimary(PrimaryContext ctx) {
+		if (ctx.IDENTIFIER() != null) {
+			return new ClassNode(ctx, ctx.IDENTIFIER().getText());
+		} else if (ctx.THIS() != null) {
+			return new ClassNode(ctx, ctx.THIS().getText());
+		} else if (ctx.SUPER() != null) {
+			return new ClassNode(ctx, ctx.SUPER().getText());
+		} else if (ctx.THIS() != null) {
+			return new ClassNode(ctx, ctx.IDENTIFIER().getText());
+		} else if (ctx.literal() != null) {
+			return visitLiteral(ctx.literal());
+		}
+
+		return super.visitPrimary(ctx);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * parse.parser.java.comp.JavaParserBaseVisitor#visitLiteral(parse.parser.
+	 * java.comp.JavaParser.LiteralContext)
+	 */
+	@Override
+	public ClassNode visitLiteral(LiteralContext ctx) {
+		if (ctx.CHAR_LITERAL() != null) {
+			return new ClassNode(ctx, ctx.CHAR_LITERAL().getText());
+		} else if (ctx.STRING_LITERAL() != null) {
+			return new ClassNode(ctx, ctx.STRING_LITERAL().getText());
+		} else if (ctx.BOOL_LITERAL() != null) {
+			return new ClassNode(ctx, ctx.BOOL_LITERAL().getText());
+		} else if (ctx.NULL_LITERAL() != null) {
+			return new ClassNode(ctx, ctx.NULL_LITERAL().getText());
+		} else if (ctx.integerLiteral() != null) {
+			return visitIntegerLiteral(ctx.integerLiteral());
+		} else if (ctx.floatLiteral() != null) {
+			return visitFloatLiteral(ctx.floatLiteral());
+		}
+
+		return super.visitLiteral(ctx);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * parse.parser.java.comp.JavaParserBaseVisitor#visitIntegerLiteral(parse.
+	 * parser.java.comp.JavaParser.IntegerLiteralContext)
+	 */
+	@Override
+	public ClassNode visitIntegerLiteral(IntegerLiteralContext ctx) {
+		if (ctx.DECIMAL_LITERAL() != null) {
+			return new ClassNode(ctx, ctx.DECIMAL_LITERAL().getText());
+		} else if (ctx.HEX_LITERAL() != null) {
+			return new ClassNode(ctx, ctx.HEX_LITERAL().getText());
+		} else if (ctx.OCT_LITERAL() != null) {
+			return new ClassNode(ctx, ctx.OCT_LITERAL().getText());
+		} else if (ctx.BINARY_LITERAL() != null) {
+			return new ClassNode(ctx, ctx.BINARY_LITERAL().getText());
+		}
+
+		return super.visitIntegerLiteral(ctx);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * parse.parser.java.comp.JavaParserBaseVisitor#visitFloatLiteral(parse.
+	 * parser.java.comp.JavaParser.FloatLiteralContext)
+	 */
+	@Override
+	public ClassNode visitFloatLiteral(FloatLiteralContext ctx) {
+		if (ctx.FLOAT_LITERAL() != null) {
+			return new ClassNode(ctx, ctx.FLOAT_LITERAL().getText());
+		} else if (ctx.HEX_FLOAT_LITERAL() != null) {
+			return new ClassNode(ctx, ctx.HEX_FLOAT_LITERAL().getText());
+		}
+
+		return super.visitFloatLiteral(ctx);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * parse.parser.java.comp.JavaParserBaseVisitor#visitPrimitiveType(parse.
+	 * parser.java.comp.JavaParser.PrimitiveTypeContext)
+	 */
+	@Override
+	public ClassNode visitPrimitiveType(PrimitiveTypeContext ctx) {
+		if (ctx.BOOLEAN() != null) {
+			return new ClassNode(ctx, ctx.BOOLEAN().getText());
+		} else if (ctx.CHAR() != null) {
+			return new ClassNode(ctx, ctx.CHAR().getText());
+		} else if (ctx.BYTE() != null) {
+			return new ClassNode(ctx, ctx.BYTE().getText());
+		} else if (ctx.SHORT() != null) {
+			return new ClassNode(ctx, ctx.SHORT().getText());
+		} else if (ctx.INT() != null) {
+			return new ClassNode(ctx, ctx.INT().getText());
+		} else if (ctx.LONG() != null) {
+			return new ClassNode(ctx, ctx.LONG().getText());
+		} else if (ctx.FLOAT() != null) {
+			return new ClassNode(ctx, ctx.FLOAT().getText());
+		} else if (ctx.DOUBLE() != null) {
+			return new ClassNode(ctx, ctx.DOUBLE().getText());
+		} else {
+			return visitChildren(ctx);
+		}
+	}
+}
